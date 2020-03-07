b88c52ae4af3322e1378c524179046119f66d0a2
Marina Zujic <zujicmarina@gmail.com>
2020-03-07 12:22:07 +0100

    Responsive toggle menu

---
+++ js/menu-toggle.js
@@ -0,0 +1,8 @@
+let mainNav = document.getElementById("js-menu");
+
+let navBarToggle = document.getElementById("nav-bar");
+
+navBarToggle.addEventListener("click", function () {
+    
+    mainNav.classList.toggle("menu-item");
+});
